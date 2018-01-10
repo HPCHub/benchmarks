de1ee0280dc2e88f0faf8f645c74013268a8b86b
Mihail Vasilev <mickvav@gmail.com>
2018-01-10 10:53:18 +0300

    Added platform with fixed 8 threads.

---
+++ platforms/local-mpich-8threads.sh
@@ -0,0 +1,38 @@
+NCPU=8
+
+export OMP_NUM_THREADS=$NCPU
+
+FFTW_CONFIGURE_FLAGS=""
+for feature in sse2 avx avx2; do
+  if grep $feature /proc/cpuinfo > /dev/null; then
+FFTW_CONFIGURE_FLAGS="${FFTW_CONFIGURE_FLAGS} --enable-$feature"
+  fi
+done
+
+export CC=`which mpicc.mpich`
+if [ ! -x "$CC" ]; then
+  export CC=`which gcc`
+fi
+if [ ! -x "$CC" ]; then
+  if [ "$HPCHUB_TEST_STATE" == "install" ]; then
+    echo "Platform error: no C compiler installed!"
+    exit 1
+  fi
+fi
+
+if [ "$HPCHUB_TEST_STATE" == "install" ]; then
+  echo "Platform: using $CC as compiler"
+fi
+
+export FFTW_CONFIGURE_FLAGS
+export HPCHUB_LINKER=`which mpif77.mpich`
+export HPCHUB_LAPACK_DIR="/usr/lib"
+
+HPCHUB_PWD=`pwd`
+export HPCHUB_MPIRUN="mpirun.mpich -np $NCPU -machinefile machinefile "
+
+if [ ! -f machinefile ]; then 
+    for i in `seq 1 $NCPU`; do
+      echo localhost >> machinefile
+    done
+fi
