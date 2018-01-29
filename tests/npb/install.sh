76ddf88af0c52c690a324dd33ea25c63e74804f6
Timur <tismagil@gmail.com>
2018-01-29 19:53:40 +0300

    npb install fixes

--- tests/npb/install.sh
+++ tests/npb/install.sh
@@ -24,13 +24,15 @@ cp ./config/make.def.template ./config/make.def
 
 sed -i 's@MPIF77 = .*@MPIF77 = '"$FC"'@' ./config/make.def
 sed -i 's@MPICC = .*@MPICC = '"$CC"'@' ./config/make.def
-sed -i 's/FMPI_LIB  = .*/FMPI_LIB  =/' ./config/make.def
-sed -i 's/FMPI_INC  = .*/FMPI_INC  =/' ./config/make.def
+sed -i 's/FMPI_LIB.*/FMPI_LIB  =/' ./config/make.def
+sed -i 's/FMPI_INC.*/FMPI_INC  =/' ./config/make.def
 
-sed -i 's/CMPI_LIB  = .*/CMPI_LIB  =/' ./config/make.def
-sed -i 's/CMPI_INC  = .*/CMPI_INC  =/' ./config/make.def
-sed -i 's@CFLAGS = .*@CFLAGS = -O3 --mcmodel=medium'" $FFTW_CONFIGURE_FLAGS"'@' ./config/make.def
-sed -i 's@FFLAGS = .*@FFLAGS = -O3 --mcmodel=medium'" $FFTW_CONFIGURE_FLAGS"'@' ./config/make.def
+sed -i 's/CMPI_LIB.*/CMPI_LIB  =/' ./config/make.def
+sed -i 's/CMPI_INC.*/CMPI_INC  =/' ./config/make.def
+sed -i 's@CFLAGS.*@CFLAGS = -O3 --mcmodel=medium'" $FFTW_CONFIGURE_FLAGS"'@' ./config/make.def
+sed -i 's@FFLAGS.*@FFLAGS = -O3 --mcmodel=medium'" $FFTW_CONFIGURE_FLAGS"'@' ./config/make.def
+sed -i 's@FLINKFLAGS.*@FLINKFLAGS = -O3@' ./config/make.def
+sed -i 's@CLINKFLAGS.*@CLINKFLAGS = -O3@' ./config/make.def
 
 #generate build suit for NPB
 
