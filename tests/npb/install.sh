8ec5fd1337a57dab164424428d83183420f3882e
Timur <tismagil@gmail.com>
2018-01-29 17:56:13 +0300

    add install for npb tests

--- tests/npb/install.sh
+++ tests/npb/install.sh
@@ -16,10 +16,53 @@ if [ ! -f NPB${npb_version}.tar.gz ]; then
   tar -xvzf NPB${npb_version}.tar.gz
 fi
 
-cd NPB${npb_version}
+cd NPB${npb_version}/NPB3.3-MPI
 
-#./configure  CC=$CC CXX=$CXX FC=$FC
-#--prefix=${HOME}/usr
+#generate config fo NPB
+cp make.def.template ./make.def
 
-make
-#make install
+sed -i 's/MPIF77 = .*/MPIF77 = $FC/' ./config/make.def
+sed -i 's/MPICC = .*/MPICC = $CC/' ./config/make.def
+sed -i 's/FMPI_LIB  = .*/FMPI_LIB  =' ./config/make.def
+sed -i 's/FMPI_INC  = .*/FMPI_INC  =' ./config/make.def
+
+sed -i 's/CMPI_LIB  = .*/CMPI_LIB  =' ./config/make.def
+sed -i 's/CMPI_INC  = .*/CMPI_INC  =' ./config/make.def
+sed -i 's/CFLAGS = .*/CFLAGS = -O3 --mcmodel=medium $FFTW_CONFIGURE_FLAGS' ./config/make.def
+sed -i 's/FFLAGS = .*/FFLAGS = -O3 --mcmodel=medium $FFTW_CONFIGURE_FLAGS' ./config/make.def
+
+#generate build suit for NPB
+
+if [ -f "${HPCHUB_MACHINEFILE}" ];then
+	cp "${HPCHUB_MACHINEFILE}" ./machinefile
+else
+	cp ../machinefile ./
+fi
+
+local_ncpus=`cat machinefile | wc -l`
+
+if [ -f suite.def ]; then
+	rm suite.def
+fi
+i=1
+while [ i -le $local_ncpus ]; do
+	echo is C $i >> suite.def
+	echo lu C $i >> suite.def
+	echo ft C $i >> suite.def
+	echo mg C $i >> suite.def
+	echo cg C $i >> suite.def
+	echo ep C $i >> suite.def
+	let i = i * 2
+done
+
+i=1
+j=1
+while [ j -le $local_ncpus ]; do
+	echo sp $i >> suite.def
+	echo bt $i >> suite.def
+	let i = i + 1
+	let j = i * i
+done
+
+make clean
+make suite
