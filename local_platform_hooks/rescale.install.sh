d2edc531efd724fb8bf899466897a8dde740db71
Mihail Vasilev <mickvav@gmail.com>
2018-01-31 11:10:47 +0300

    Rescale first alpha.

--- local_platform_hooks/rescale.install.sh
+++ local_platform_hooks/rescale.install.sh
@@ -3,7 +3,10 @@
 echo "Rescale platform is weird, and has no SSH access."
 echo "Thus you will have to do some stuff in your web browser."
 echo "First, I'll create a tarball for you:"
-git archive --format tar.gz master > master.tar.gz
+git archive --format tar master > master.tar
+tar -rf master.tar `find -name '.disable*'`
+gzip master.tar
+
 echo "Here it is:"
 ls -la master.tar.gz
 echo "Now, create a new job in https://platform.rescale.com/jobs/new-job/"
@@ -15,10 +18,34 @@ tar -xvzf master.tar.gz
 for i in tests/*; do
   cd \$i 
   testname=\${i##tests/}
-  HPCHUB_PLATFORM=../../platforms/rescale.sh ./install.sh
-  HPCHUB_OPERATION=run HPCHUB_REPORT=\`pwd\`/report.\$testname.txt HPCHUB_PLATFORM=../../platforms/rescale.sh ./run.sh
+  if [ ! -f \$i/.disable_install ]; then
+    HPCHUB_PLATFORM=../../platforms/rescale.sh ./install.sh
+  fi
+  if [ ! -f \$i/.disable_run ]; then 
+    HPCHUB_OPERATION=run HPCHUB_REPORT=\`pwd\`/report.\$testname.txt HPCHUB_PLATFORM=../../platforms/rescale.sh ./run.sh
+  fi
 done
 
 EOF
+
+echo "When the job will be done, drop the files report.TESTNAME.txt in current directory:"
+pwd
+echo "And press enter (press Ctrl-C now to exit)"
+read a
+
+now=`date +%Y-%m-%d_%H:%M:%S`
+for i in tests/*; do
+  testname=${i##tests/}
+  if [ -f report.$testname.txt ]; then
+    echo "Ok, I see report.$testname.txt"
+    resdir="runs/${operation}/${testname}/${platform}/${remhost}/${now}"
+    mkdir -p "$resdir"
+    echo "I will move it into $resdir/report.time.txt"
+    mv report.$testname.txt $resdir/report.time.txt
+  else
+    echo "Ups, I don't see report.$testname.txt"
+    echo "Hope it's OK"
+  fi
+done
 
 exit
