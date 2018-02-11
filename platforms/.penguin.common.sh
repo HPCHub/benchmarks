69c2311f1a148d4e20af31069a1725e616a32cff
Mihail Vasilev <mickvav@gmail.com>
2018-02-11 10:59:56 +0300

    Pre-initial (not tested, written by specs) penguin
    issue#2
    
    Written according to https://pod.penguincomputing.com/documentation/running_mpi_Jobs
    
    NOT TESTED at this moment.

---
+++ platforms/.penguin.common.sh
@@ -0,0 +1,92 @@
+
+NNODES=4
+
+NCPU=48
+
+export OMP_NUM_THREADS=12
+
+FFTW_CONFIGURE_FLAGS=""
+for feature in sse2 avx avx2; do
+  if grep $feature /proc/cpuinfo > /dev/null; then
+FFTW_CONFIGURE_FLAGS="${FFTW_CONFIGURE_FLAGS} --enable-$feature"
+  fi
+done
+
+HPCHUB_HAS_CPUSET=1
+
+export CC=`which mpicc`
+if [ ! -x "$CC" ]; then
+  export CC=`which gcc`
+fi
+if [ ! -x "$CC" ]; then
+  if [ "$HPCHUB_TEST_STATE" == "install" ]; then
+    echo "Platform error: no C compiler installed!"
+    exit 1
+  fi
+fi
+
+export CXX=`which mpicxx`
+if [ ! -x "$CXX" ]; then
+  export CXX=`which g++`
+fi
+if [ ! -x "$CXX" ]; then
+  if [ "$HPCHUB_TEST_STATE" == "install" ]; then
+    echo "Platform error: no CXX compiler installed!"
+    exit 1
+  fi
+fi
+
+export FC=`which mpif90`
+if [ ! -x "$FC" ]; then
+  export FC=`which mpif90`
+fi
+if [ ! -x "$FC" ]; then
+  if [ "$HPCHUB_TEST_STATE" == "install" ]; then
+    echo "Platform error: no FC compiler installed!"
+    exit 1
+  fi
+fi
+
+if [ "$HPCHUB_TEST_STATE" == "install" ]; then
+  echo "Platform: using $CC as compiler"
+fi
+
+if [ "$HPCHUB_OPERATION" == "install_system" ]; then
+  echo YUM:
+  
+  sudo yum -y install atlas cmake blas-devel
+  for i in $NODES; do
+    ssh $i  sudo yum -y install atlas cmake blas-devel
+  done
+fi
+export FFTW_CONFIGURE_FLAGS
+export HPCHUB_LINKER=`which mpif77`
+export HPCHUB_LAPACK_DIR="/usr/lib"
+
+HPCHUB_PWD=`pwd`
+
+function hpchub_mpirun {
+    L=`qstat | wc -l`
+    HPCHUB_PPN=$((NCPU/NNODES))
+    cat > _mpirun_hpchub.pbs <<EOF
+#PBS -q FREE
+#PBS -l nodes=$NODES:ppn=$HPCHUB_PPN
+#PBS -l walltime=00:01:00
+#PBS -S /bin/bash
+#PBS -o $HPCHUB_PWD/_mpirun_hpchub.stdout
+#PBS -e $HPCHUB_PWD/_mpirun_hpchub.stderr
+module load openmpi/1.5.5/gcc.4.4.6
+mpirun $@
+EOF
+    qsub _mpirun_hpchub.pbs
+    while [ `qstat | wc -l` -gt "$L" ]; do
+      sleep 1
+    done
+    echo "hpchub_mpirun stdout:"
+    cat $HPCHUB_PWD/_mpirun_hpchub.stdout
+    echo "hpchub_mpirun stderr:"
+    cat $HPCHUB_PWD/_mpirun_hpchub.stderr
+}
+
+export HPCHUB_MPIRUN="hpchub_mpirun"
+
