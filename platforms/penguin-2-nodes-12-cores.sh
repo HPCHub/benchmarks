3b31b42a5c495843d9dcceb69148ceb001e32bc9
Mihail Vasilev <mickvav@gmail.com>
2018-02-12 21:16:32 +0300

    Penguin - added 2 nodes variant

---
+++ platforms/penguin-2-nodes-12-cores.sh
@@ -0,0 +1,14 @@
+
+NNODES=2
+NCPU=48
+
+if [ -f "$HPCHUB_PLATFORM" ]; then
+  HPCHUB_PLATFORM_COMMON=${HPCHUB_PLATFORM%%penguin-4-nodes-12-cores.sh}.penguin.common.sh
+else
+  HPCHUB_PLATFORM_COMMON=../.penguin.common.sh
+fi
+
+if [ ! -f ${HPCHUB_PLATFORM_COMMON} ]; then
+  echo "Penguin common platform file not found! Can't resume."
+  exit 1
+fi
