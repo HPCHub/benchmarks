35938b72af504b837aa58c386c9eac56ad465d92
tismagil@gmail.com <tismagil@gmail.com>
2018-02-16 14:15:19 +0300

    minor fixes

--- tests/osu/install.sh
+++ tests/osu/install.sh
@@ -38,7 +38,7 @@ ${HPCHUB_COMPILE_PREFIX} ./configure  CC=$MPICC CXX=$MPICXX FC=$MPIFC
 ${HPCHUB_COMPILE_PREFIX} make
 if [ $HPCHUB_PLATFORM == 'azure' ]; then
 	for i in $NODES; do
-		scp -r ../osu-micro-benchmarks-${osu_version}/  $i:$HPCHUB_PWD
+		scp -r ../osu-micro-benchmarks-${osu_version}/  $i:$HOME/hpchub_benchmark
 	done 
 fi
 #make install
