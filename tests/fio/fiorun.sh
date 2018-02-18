f5fad970212e05a0b8345e9f1332db429b378786
Mihail Vasilev <mickvav@gmail.com>
2018-02-18 18:54:12 +0300

    Added different file sizes.

--- tests/fio/fiorun.sh
+++ tests/fio/fiorun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 op=$1
-
+size=$2
 (
  if flock -n 200; then
-  ${HOME}/usr/bin/fio --name=global --rw=$op --size=128m --output=job.${HOSTNAME} --name=box.${HOSTNAME} 
+  ${HOME}/usr/bin/fio --name=global --rw=$op --size=$size --output=job.${HOSTNAME} --name=box.${HOSTNAME} 
  else 
   flock -x 200
  fi
