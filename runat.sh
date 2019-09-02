d1f32a036a814621ced7540e89e9bbbb9b9b626f
Timur Ismagilov <tismagil@gmail.com>
2019-09-02 20:08:27 +0000

    add single node mode

--- runat.sh
+++ runat.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-if [ "$1" = "" -o ! -f platforms/${2}.sh -o "$3" = "" ]; then
+# $1 command
+function rexec
+{
+	if [ "$islocal" = "1" ]; then
+		$1
+	else
+		$remcomm "$1"
+	fi
+}
+
+if [ "$1" = "" -o ! -f "platforms/${2}.sh" -o "$3" = "" ]; then
   echo "Usage:"
   echo "  runat.sh [user@]HOST PLATFORM OPERATION [test]"
   echo "where"
@@ -18,21 +28,43 @@ if [ ! -d "runs" ]; then
   mkdir runs
 fi
 
-remhost=$1
-platform=$2
-operation=$3
-testopt=$4
+hpchub_benchmark_dir="hpchub_benchmark"
+
+remhost="$1"
+platform="$2"
+operation="$3"
+testopt="$4"
+
+platform_hook_nfs_dir=""
 
 if [ -f "local_platform_hooks/$platform.$operation.sh" ];then
   . local_platform_hooks/$platform.$operation.sh
 fi
 
-remwd=`ssh $remhost pwd`
-if [ ! "$?" = "0" -o "$remwd" = "" ]; then
-  echo "Problems connecting to host $remhost"
-  exit 2
+islocal="0"
+remcomm="ssh $remhost"
+
+if [ "$remhost" = "localhost" ]; then
+  remhost="$(hostname)"
+  islocal="1"
+  curr_wd="$(pwd)"
+  script_path="$0"
+  full_path="$curr_wd/$script_path"
+  remwd="$(realpath $(dirname "$full_path"))"
+  hpchub_benchmark_dir="$(basename $remwd)"
+  remwd="$(dirname "$remwd")"
+  remcomm="eval"
 fi
-remwd=$remwd/$platform_hook_nfs_dir
+
+
+if [ "$islocal" != "1" ]; then
+	remwd="$($remcomm pwd)"
+	if [ ! "$?" = "0" -o "$remwd" = "" ]; then
+		echo "Problems connecting to host $remhost"
+		exit 2
+	fi
+	remwd=$remwd/$platform_hook_nfs_dir
+fi
 
 if [ "$testopt" = "" ]; then
   testset=tests/*
@@ -41,43 +73,68 @@ else
 fi
 
 if [ "$operation" = "install" ]; then
-  git archive --format tar.gz master | ssh $remhost "cat > $remwd/hpchub_benchmark.tar.gz"
-  ssh $remhost "mkdir -p $remwd/hpchub_benchmark" 
-  ssh $remhost "cd $remwd/hpchub_benchmark; tar -xvzf ../hpchub_benchmark.tar.gz" || exit 4
-  echo "tarballs sent."
-  for i in $testset; do
-    if [ -d "$i" -a ! "$i" = "tests/include" -a ! -f "$i/.disable_install" ]; then 
-      ssh $remhost "cd $remwd/hpchub_benchmark/$i; HPCHUB_PLATFORM=../../platforms/${platform}.sh ./install.sh" || exit 5
+  if [ "$islocal" = "0" ]; the
+    tar -czf - . | ssh $remhost "cat > $remwd/hpchub_benchmark.tar.gz"
+    #git archive --format tar.gz master | ssh $remhost "cat > $remwd/hpchub_benchmark.tar.gz"
+    $remcomm "mkdir -p $remwd/${hpchub_benchmark_dir}" 
+    $remcomm "cd $remwd/${hpchub_benchmark_dir}; tar -xvzf ../hpchub_benchmark.tar.gz" || exit 4
+    echo "tarballs sent."
+  else
+    echo "Running on the localhost. Tarball sending will be skipped"
+  fi
+
+  for ctest in $testset; do
+    if [ -d "$ctest" -a ! "$ctest" = "tests/include" -a ! -f "$ctest/.disable_install" ]; then
+      $remcomm "cd $remwd/${hpchub_benchmark_dir}/$ctest; HPCHUB_ISLOCAL=$islocal HPCHUB_PLATFORM=../../platforms/${platform}.sh ./install.sh" || exit 5
     fi
   done
-  ssh $remhost "echo ok > $remwd/hpchub_benchmark/install_ok"
+
+  $remcomm "echo ok > $remwd/${hpchub_benchmark_dir}/install_ok"
 
 elif [ "$operation" = "clean" ];then
+  if [ "$islocal" = "1" ]; then
+    echo "FIXIT. The cleanup action on the local machine may be unsafe"
+    exit 0
+  fi
+
   for i in $testset; do
     if [ -x "$i/clean.sh" -a ! "$i" = "tests/include" ]; then 
-       ssh $remhost "cd hpchub_benchmark/$i; HPCHUB_PLATFORM=../../platforms/${platform}.sh ./clean.sh"
+       $remcomm "cd $remwd/${hpchub_benchmark_dir}/$i; HPCHUB_PLATFORM=../../platforms/${platform}.sh ./clean.sh"
     fi
   done
-  ssh $remhost "rm -rf hpchub_benchmark"
-else
+  $remcomm "rm -rf $remwd/${hpchub_benchmark_dir}"
+
+elif [ "$operation" = "run" ]; then
   now=`date +%Y-%m-%d_%H:%M:%S`
   
-  for i in $testset; do
-    if [ -x "$i/${operation}.sh" -a ! "$i" = "tests/include" -a ! -f "$i/.disable_run" ]; then 
-       testname=${i##tests/}
-       resdir="runs/${operation}/${testname}/${platform}/${remhost}/${now}"
-       mkdir -p "$resdir"
-       remreport=$remwd/hpchub_benchmark/${operation}_${testname}_${now}.log
+  for ctest in $testset; do
+    if [ -x "$ctest/${operation}.sh" -a ! "$ctest" = "tests/include" -a ! -f "$ctest/.disable_run" ]; then 
+      testname=${ctest##tests/}
+      resdir="runs/${operation}/${testname}/${platform}/${remhost}/${now}"
+      mkdir -p "$resdir"
+      remreport=$remwd/${hpchub_benchmark_dir}/${operation}_${testname}_${now}.log
 
-       echo "Runing test: $testname"
-       echo "expecting remote host $remhost to generate report at: ${remreport}"
+      echo "Runing test: $testname"
+      echo "expecting remote host $remhost to generate report at: ${remreport}"
 
-       ssh $remhost "cd $remwd/hpchub_benchmark/$i; HPCHUB_OPERATION=${operation} HPCHUB_REPORT=${remreport} HPCHUB_RESDIR=${resdir} HPCHUB_PLATFORM=../../platforms/${platform}.sh ./${operation}.sh" 2>&1 | tee $resdir/out.log
-       scp $remhost:$remreport $resdir/report.time.txt || echo "report time not logged"
-       if [ $testname == 'npb' -o $testname == 'osu' ]; then
-           echo "Also fetching additional files: "
-           scp $remhost:$remwd/hpchub_benchmark/$resdir/* $resdir
-       fi
+      $remcomm "cd $remwd/${hpchub_benchmark_dir}/$ctest;
+                HPCHUB_OPERATION=${operation}
+                HPCHUB_REPORT=${remreport}
+                HPCHUB_RESDIR=${resdir}
+				HPCHUB_ISLOCAL=${islocal}
+                HPCHUB_PLATFORM=../../platforms/${platform}.sh
+                 ./${operation}.sh" 2>&1 | tee $resdir/out.log
+
+      if [ "$islocal" = "0" ]; then
+        scp $remhost:$remreport $resdir/report.time.txt || echo "report time not logged"
+        if [ $testname == 'npb' -o $testname == 'osu' ]; then
+            echo "Also fetching additional files: "
+            scp $remhost:$remwd/${hpchub_benchmark_dir}/$resdir/* $resdir
+        fi
+      fi
     fi
   done
+else
+    echo "Undefined operation \"$operations\": avalible operations is install run clean"
+    exit 1
 fi
