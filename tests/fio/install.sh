d0fe6007309f367b4d3c9d94f2f0f9acd3e96f18
Oracle Public Cloud User <opc@master11.private.a1.oraclevcn.com>
2019-09-03 19:05:38 +0000

    Fix bugs, add instance mode

--- tests/fio/install.sh
+++ tests/fio/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ -f ../platform.sh ]; then
   . ../platform.sh
 fi
@@ -8,18 +7,32 @@ fi
 HPCHUB_TEST_STATE=install
 
 if [ -f "${HPCHUB_PLATFORM}" ]; then
-  . ${HPCHUB_PLATFORM}
+  . "${HPCHUB_PLATFORM}"
 fi
 
-if [ ! -f ${fio_fname} ]; then
-  wget ${fio_url}
-  tar -xvzf ${fio_fname}
+if [ ! -f "${fio_fname}" ]; then
+  wget "${fio_url}"
+  tar -xvzf "${fio_fname}"
 fi
 
-cd ${fio_dir}
+test_dir=$(pwd)
+hpchub_benchmark_dir="$(realpath "$test_dir/../../")"
 
-${HPCHUB_COMPILE_PREFIX} ./configure --prefix=${HOME}/usr
+cd "${fio_dir}"
+
+${HPCHUB_COMPILE_PREFIX} ./configure --prefix="$test_dir/install"
 ${HPCHUB_COMPILE_PREFIX} make
-${HPCHUB_COMPILE_PREFIX} make install
+#${HPCHUB_COMPILE_PREFIX} make install
 
+chmod a+x "$test_dir/fiorun.sh"
+cp "$test_dir/fiorun.sh" ./
 
+PLATFORM_NAME="$(basename "$HPCHUB_PLATFORM" | sed -e "s/\..*//" )"
+if [ "$HPCHUB_ISLOCAL" != "1" -a "$HPCHUB_ISNFS" != "1" ]; then
+    if [ "$PLATFORM_NAME" = "azurer" -o "$PLATFORM_NAME" = "OCI" ]; then
+        for i in $NODES; do
+            ssh $i "mkdir -p $hpchub_benchmark_dir"
+            scp -r ../../../tests  "$i:$hpchub_benchmark_dir"
+        done
+    fi
+fi
